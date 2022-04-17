72b303d34355fab4ab47392f4399dd09bd8a9c65
lucette25 <hermione.odjo@imsp-uac.org>
2022-04-17 17:46:25 +0300

    phonebook_backend_step10

--- phonebookfrontend/src/services/persons.js
+++ phonebookfrontend/src/services/persons.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 
-const baseUrl = 'http://localhost:3001/api/persons'
+const baseUrl = 'https://peaceful-everglades-70618.herokuapp.com/api/persons'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
